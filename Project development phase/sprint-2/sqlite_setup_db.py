11f76c57e6a456d6761835f1c592b570a29b098a
Vishal S <102234949+svisu1063@users.noreply.github.com>
2022-11-19 07:46:44 +0530

    Delete Project development phase/sprint-2 directory

--- Project development phase/sprint-2/sqlite_setup_db.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('nutrition_database.db')
-print("Opened database successfully")
-
-conn.execute('CREATE TABLE nutrition ( username TEXT, email TEXT, password TEXT )')
-print("Table created successfully")
-conn.close()
